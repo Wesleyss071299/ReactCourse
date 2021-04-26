288c5ae4c470044a5f2362bda2a246bab8fd18be
wesley <wesleysilvadesousa@gmail.com>
2021-04-26 12:21:51 -0300

    Input validation

---
+++ 16-starting-project/src/components/SimpleInput.js
@@ -0,0 +1,52 @@
+import useInput from '../hooks/use-input'
+
+const SimpleInput = (props) => {
+
+  const { 
+    value: enteredName, 
+    isValid: enteredNameIsValid,
+    hasError: nameInputHasError, 
+    valueChangeHandler: nameChangeHandler, 
+    inputBlurHandler: nameBlurHandler,
+    reset: resetNameInput
+  } = useInput(value => value.trim() !== '');
+
+  let formIsValid = false;
+
+    if (enteredNameIsValid) {
+      formIsValid = true
+    }
+
+  const formSubmissionHandler = (event) => {
+    event.preventDefault();
+
+    if (!enteredNameIsValid) {
+      return;
+    }
+
+    resetNameInput();
+  }
+
+  const nameInputClasses = nameInputHasError ? 'form-control invalid' : 'form-control'
+
+  return (
+    <form onSubmit={formSubmissionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name</label>
+        <input 
+          type='text' 
+          id='name' 
+          onChange={nameChangeHandler}
+          onBlur={nameBlurHandler}
+          value={enteredName} 
+        />
+      </div>
+      { nameInputHasError && <p className="error-text">Name must not be empty</p>}
+      <div className="form-actions">
+        <button disabled={!formIsValid}>Submit</button>
+      </div>
+    </form>
+  );
+};
+
+export default SimpleInput;
