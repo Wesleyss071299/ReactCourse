032bb7fe40cab6f3ff3b177de30bb1040cc02872
wesley <wesleysilvadesousa@gmail.com>
2021-04-28 16:26:38 -0300

    feat: redux login example

--- 18-starting-project/src/store/index.js
+++ 18-starting-project/src/store/index.js
@@ -1,20 +1,49 @@
 import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
 
-const counterReducer = (state = { conter: 0 }, action) => {
-    if (action.type === 'increment') {
-        return {
-            counter: state.counter + 1
-        }
+
+import counterReducer from './counter-slice'
+import authReducer from './auth-slice'
+
+
+
+// const counterReducer = (state = initialState, action) => {
+//     if (action.type === 'increment') {
+//         return {
+//             counter: state.counter + 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+//     if (action.type === 'increase') {
+//         return {
+//             counter: state.counter + action.amount,
+//             showCounter: state.showCounter  
+//         }
+//     }
+//     if (action.type === 'decrement') {
+//         return {
+//             counter: state.counter - 1,
+//             showCounter: state.showCounter
+//         }
+//     }
+//     if (action.type === 'toogle') {
+//         return {
+//             counter: state.counter,
+//             showCounter: !state.showCounter
+//         }
+//     }
+
+//     return state;
+// }
+
+// const store = createStore(counterReducer);
+
+const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+        auth: authReducer
     }
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1
-        }
-    }
+})
 
-    return state;
-}
-
-const store = createStore(counterReducer);
 
 export default store;
